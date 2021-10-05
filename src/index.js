3a9e21b4e72ca5d2780fd6a94b4a4299e29cd846
Eugene Kravec <krava255@gmail.com>
2021-10-05 22:29:05 +0300

    add templates

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-import './menu.json'
-import styles from './styles.css'
+import menuArr from './menu.json'
+import { template } from '../templates/cards.hbs'
 
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
 
+const menuList = document.querySelector('.js-menu')
+const markUp = template(menuArr)
+
+menuList.insertAdjacentHTML('beforeend', markUp)
+console.log(1);
